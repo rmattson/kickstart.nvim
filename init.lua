d5f7668b86dd672b71f6c8d962ba5f0a87c432f1
unknown <rmattson@gmail.com>
2024-08-26 11:11:02 -0700

    Setting explicit softtabstop, shiftwidth, expandtab

--- init.lua
+++ init.lua
@@ -157,6 +157,11 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- RGM: tab settings
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
